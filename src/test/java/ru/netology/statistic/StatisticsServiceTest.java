de0324c4029e787b6bfd4b0e3415945ccaeeb516
Nikita Aniskovets <happyaggro@gmail.com>
2021-04-22 18:46:43 +0300

    Coverage 85%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,15 +18,15 @@ class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
 
-    @Test
-    void findMax2() {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {3, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMax(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
+//    @Test
+//    void findMax2() {
+//        StatisticsService service = new StatisticsService();
+//
+//        long[] incomesInBillions = {3, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+//        long expected = 12;
+//
+//        long actual = service.findMax(incomesInBillions);
+//
+//        assertEquals(expected, actual);
+//    }
 }
